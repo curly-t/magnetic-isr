0989c8332e6e39e6b12f7f14145874c149948fe9
curly-t <turn.janez@gmail.com>
2021-10-15 13:11:09 +0200

    Updated the get rod and get calibration scripts to include rod and tub information

--- misr/utils/get_rod_and_tub_info.py
+++ misr/utils/get_rod_and_tub_info.py
@@ -2,15 +2,15 @@ import numpy as np
 from tkinter.filedialog import askopenfilename
     
 def get_rod_info(initialdir=None):
-    filepath = askopenfilename(initialdir=initialdir)
+    filepath = askopenfilename(initialdir=initialdir, title="Select Rod info file")
     # The fields are: (every value written in each row, all in one collumn)
     # id, L, m, d, L_err, m_err, d_err         (id, Length, mass, diameter)
     # Additional comments are saved in a comment
     return np.loadtxt(filepath)
 
 def get_tub_info(initialdir=None):
-    filepath = askopenfilename(initialdir=initialdir)
+    filepath = askopenfilename(initialdir=initialdir, title="Select Tub info file")
     # The fields are: (every value written in each row, all in one collumn)
-    # id, W, h, V         (id, Width, hight, Volume (NOT total but water volume used [ml]))
+    # id, W, h, V, W_err, h_err, V_err         (id, Width, hight, Volume (NOT total but water volume used  [SI units]))
     # Additional comments are saved in a comment
     return np.loadtxt(filepath)
--- misr/utils/save_and_get_calibration.py
+++ misr/utils/save_and_get_calibration.py
@@ -1,15 +1,20 @@
 import numpy as np
 from tkinter.filedialog import asksaveasfilename, askopenfilename
 
-def save_simple_calibration(calibration_arr, initialdir=None):
+def save_simple_calibration(calibration_arr, rod_info, tub_info, initialdir=None):
     filepath = asksaveasfilename(initialdir=initialdir)
     # The fields are:
-    # alpha, k, c, alpha_err, k_err, c_err
-    np.savetxt(filepath, calibration_arr)
+    # alpha, k, c, alpha_err, k_err, c_err, rod_id, rod_L, rod_m, rod_d, rod_L_err, rod_m_err, rod_d_err, tub_id, tub_W, tub_h, tub_V, tub_W_err, tub_h_err, tub_V_err
+    # First six values are calibration constants, then the rod: id, Length, mass, diameter (and errors), then tub: id, Width, total height, water volume (and errors) [SI units]
+    np.savetxt(filepath, np.concatenate([calibration_arr, rod_info, tub_info]))
     
 def get_simple_calibration(initialdir=None):
     filepath = askopenfilename(initialdir=initialdir)
     # The fields are:
-    # alpha, k, c, alpha_err, k_err, c_err
-    return np.loadtxt(filepath)
-    
+    # alpha, k, c, alpha_err, k_err, c_err, rod_id, rod_L, rod_m, rod_d, rod_L_err, rod_m_err, rod_d_err, tub_id, tub_W, tub_h, tub_V, tub_W_err, tub_h_err, tub_V_err
+    # First six values are calibration constants, then the rod: id, Length, mass, diameter (and errors), then tub: id, Width, total height, water volume (and errors) [SI units]
+    cal_info = np.loadtxt(filepath)
+    calibration_array = cal_info[:6]
+    rod_info = cal_info[6:13]
+    tub_info = cal_info[13:]
+    return calibration_array, rod_info, tub_info
