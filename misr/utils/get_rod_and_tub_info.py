c3b3b28d8cbcc21add9262d011d8fae31b3d8475
curly-t <turn.janez@gmail.com>
2021-10-15 12:42:04 +0200

    Added get rod and tub info functionality.

---
+++ misr/utils/get_rod_and_tub_info.py
@@ -0,0 +1,19 @@
+import numpy as np
+from tkinter.filedialog import askopenfilename
+    
+def get_rod_info(initialdir=None):
+    filepath = askopenfilename(initialdir=initialdir)
+    # The fields are: (every value written in each row, all in one collumn)
+    # id, L, m, d, L_err, m_err, d_err         (id, Length, mass, diameter)
+    # Additional comments are saved in a comment
+    return np.loadtxt(filepath)
+
+def get_tub_info(initialdir=None)
+    filepath = askopenfilename(initialdir=initialdir)
+    # The fields are: (every value written in each row, all in one collumn)
+    # id, W, h, V         (id, Width, hight, Volume (NOT total but water volume used [ml]))
+    # Additional comments are saved in a comment
+    return np.loadtxt(filepath)
+
+
+    
