cbd9ec886da26297900a487f66387b48b423f49c
curly-t <turn.janez@gmail.com>
2021-10-19 22:28:23 +0200

    Added simple flowfield visualisation

---
+++ misr/plotting/flowfield_FDM.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def visualize_flowfield(flowfield, ps, thetas)
+    g = np.transpose(flowfield)
+
+    fig = plt.figure()
+    rad = np.exp(ps)
+    azm = thetas + 3/2 * np.pi
+    r, th = np.meshgrid(rad, azm)
+    z = np.real(g)
+
+    plt.subplot(projection="polar")
+    plt.pcolormesh(th, r, z, shading="gouraud")
+    plt.pcolormesh(th - np.pi/2, r, z[::-1], shading="gouraud")     # ZRCALNA SLIKA
+    plt.grid()
+    plt.show()
