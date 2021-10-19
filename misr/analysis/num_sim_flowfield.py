d9b8d746e167445028d18215298e331d9857c52a
curly-t <turn.janez@gmail.com>
2021-10-19 19:20:24 +0200

    Minor bugfixes in numerical simulation of NS equations.

--- misr/analysis/num_sim_flowfield.py
+++ misr/analysis/num_sim_flowfield.py
@@ -1,18 +1,11 @@
 import math as m
 import numpy as np
-
-
-import numpy as np
-from numpy.linalg import eigh
-from scipy.sparse.linalg import eigsh
-from scipy.sparse.linalg import eigs as eigs_nonSym
 from scipy.sparse import csr_matrix
-import matplotlib.pyplot as plt
-
 
 def FDM_matrix(N, ps, hp, htheta, Re, Bo):
-    """Constructs the N x N matrix M for solving the problem:
+    """Constructs the N x N matrix M* for solving the problem:
             M g - i Re e^2p g = c
+            M* g = c
        which represents the solution to the Navier Stokes eq (with BC).
        FDM ... Finite Difference Method """
     diag_ps = np.ones((N+2)**2) * -2. / (hp*hp)
@@ -42,7 +35,7 @@ def FDM_matrix(N, ps, hp, htheta, Re, Bo):
     bc2_2 = (np.arange(2*N+3, (N+2)**2 - (N+2), N+2), np.arange(2*N+2, (N+2)**2 - (N+2), N+2))
     bc2_3 = (np.arange(2*N+3, (N+2)**2 - (N+2), N+2), np.arange(2*N+3 + N+2, (N+2)**2, N+2))
     bc2_4 = (np.arange(2*N+3, (N+2)**2 - (N+2), N+2), np.arange(2*N+3 - (N+2), (N+2)**2 - 2*(N+2), N+2))
-    M[bc2_1] = -(2./(htheta**2) + 2./(hp**2) + 4.*Bo*np.exp(-ps[1:-1])/(2. * htheta*hp*hp))
+    M[bc2_1] = -(2./(htheta**2) + 2./(hp**2) + 4.*Bo*np.exp(-ps[1:-1])/(htheta*hp*hp))
     M[bc2_2] = 2. / (htheta**2)
     M[bc2_3] = 1./(hp**2) + 2.*Bo*np.exp(-ps[1:-1])/(hp*hp*htheta) - Bo*np.exp(-ps[1:-1])/(hp*htheta)
     M[bc2_4] = 1./(hp**2) + 2.*Bo*np.exp(-ps[1:-1])/(hp*hp*htheta) + Bo*np.exp(-ps[1:-1])/(hp*htheta)
@@ -56,19 +49,5 @@ def FDM_matrix(N, ps, hp, htheta, Re, Bo):
     c = np.zeros((N+2)**2)
     c[:N+2] = 1.0
     
+    # Mogoče deluje pravilno? Nevem čist točno?
     return csr_matrix(M - B), c
-
-
-def solve_Eigenvalue_problem_numpy(N):
-    vals, vects = eigh(FDM_matrix(N))
-    eig_vects = []
-    for i in range(len(vects[0])):
-        eig_vects.append(vects[:, i])
-    return vals, eig_vects
-
-def solve_Eigenvalue_problem_sparse(N):
-    vals, vects = eigs_nonSym(FDM_matrix(N), k=1, which='SM')
-    eig_vects = []
-    for i in range(len(vects[0])):
-        eig_vects.append(vects[:, i])
-    return np.abs(vals), np.abs(eig_vects)
