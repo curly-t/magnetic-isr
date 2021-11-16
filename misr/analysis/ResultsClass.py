c2ea9fcf4dcadda9de070bca6e50edbd53290ef7
Janez Turnšek <turn.janez@gmail.com>
2021-11-16 12:55:51 +0100

    Added most simple complex modulus estimation

--- misr/analysis/ResultsClass.py
+++ misr/analysis/ResultsClass.py
@@ -1,5 +1,6 @@
 from numpy import pi as np_pi
 
+
 class SingleResult:
     def __init__(self, result_dict, measurement_object, exceptable_phase_insanity):
         # This class is used to store the results from a single measurement
@@ -22,10 +23,13 @@ class SingleResult:
         # AR is a ratio, so the error is calculated as the sum of two relative errors
         self.AR_err = (self.rod_ampl_err/self.rod_ampl + self.meas.Iampl_err/self.meas.Iampl) * self.AR
 
-        # Initially unitialized complex modulus of the SingleResult
-        complex_modulus = None
-
     def phase_sanity_check(self):
         # Returns False (0) if there is no error, and True (1) if there is a phase error
         return not (self.exceptable_phase_insanity > self.rod_phase > (- np_pi - self.exceptable_phase_insanity))
 
+
+class FinalResults:
+    def __init__(self, cplx_Gs, single_results, calibration):
+        self.G = cplx_Gs
+        self.SRs = single_results
+        self.cal = calibration
--- misr/analysis/calc_complex_modulus.py
+++ misr/analysis/calc_complex_modulus.py
@@ -1,17 +1,9 @@
 import numpy as np
-from ..utils.save_and_get_calibration import get_simple_calibration
 from .freq_and_phase_extract import select_and_analyse
-from .calc_system_calibration import simple_calibration
+from .ResultsClass import FinalResults
 
 
-def calc_simple_complex_modulus(**kwargs):
-    cal_stat = str(input("Use previous calibration (type 'p') or calculate a new one (type 'n'): "))
-    if cal_stat == "n":
-        cal = simple_calibration(**kwargs)
-    
-    elif cal_stat == "p":
-        cal = get_simple_calibration()
-
+def calc_simple_complex_modulus(cal, **kwargs):
     # TODO: Incorporate ERRORS!!!!!!!!!!!!!!!!!!! - from here on out no errors were considered!!!!!!
 
     measured_responses = select_and_analyse(**kwargs)
@@ -20,13 +12,21 @@ def calc_simple_complex_modulus(**kwargs):
         return cal.alpha/(np.sqrt((cal.c * 2*np.pi*nu)**2 + (cal.k - cal.rod.m * 4.*np.pi*np.pi*nu*nu)**2))
 
     def phase_func(nu):
-        return np.arctan2(-2*np.pi*nu * cal.c, cal.k - cal.m * (2*np.pi*nu)**2)
+        return np.arctan2(-2*np.pi*nu * cal.c, cal.k - cal.rod.m * (2*np.pi*nu)**2)
 
     # DIRECT LINEARNO ODŠTEJEŠ EFEKT SISTEMA OD EFEKTA PROTEINA - SIMPLEST CASE
-    for resp in measured_responses:
-        resp.complex_modulus = (cal.tub.W/(4.0 * cal.rod.L)) * cal.alpha * (np.exp(-1.j*resp.rod_phase) / resp.AR -
+    cplx_Gs = np.zeros((len(measured_responses,)), dtype=np.complex128)
+    Bo = np.zeros((len(measured_responses),))
+    excld_ans = ""
+    for i, resp in enumerate(measured_responses):
+        cplx_Gs[i] = (cal.tub.W/(4.0 * cal.rod.L)) * cal.alpha * (np.exp(-1.j*resp.rod_phase) / resp.AR -
                                                                             np.exp(-1.j*phase_func(resp.rod_freq)) / AR_func(resp.rod_freq))
+        Bo[i] = AR_func(resp.rod_freq) / resp.AR
 
-    # TODO: Dodaj opozorilo da to deluje le pri visokih Bousinesqovih številih (Bo > 100)
+        if Bo[-1] < 100 and excld_ans == "":
+            print("WARNING - Results may be inconclusive as the Bo < 100 sometimes!")
+            excld_ans = str(input("Exclude inconclusive results? [Y/n] "))
+            if excld_ans == "Y":
+                continue
 
-    return measured_responses
+    return FinalResults(cplx_Gs, measured_responses, cal)
