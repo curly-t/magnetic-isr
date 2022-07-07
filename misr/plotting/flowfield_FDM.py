a0a385d4fd27ade129b7565869e2b2ce3faa5385
Janez Turnšek <turn.janez@gmail.com>
2022-07-07 20:48:05 +0200

    Dodelal plotting za FDM

--- misr/plotting/flowfield_FDM.py
+++ misr/plotting/flowfield_FDM.py
@@ -2,19 +2,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def visualize_flowfield_polar(flowfield, ps, thetas):
+def visualize_flowfield_polar(flowfield, ps, thetas, name=""):
     g = np.transpose(flowfield)
 
+    print("Real")
     fig = plt.figure()
     rad = np.exp(ps)
-    azm = thetas + 3/2 * np.pi
+    azm = thetas + 3 / 2 * np.pi
     r, th = np.meshgrid(rad, azm)
     z = np.real(g)
 
-    plt.subplot(projection="polar")
+    plt.subplot(projection="polar", frameon=False)
     plt.pcolormesh(th, r, z, shading="gouraud")
-    plt.pcolormesh(th - np.pi/2, r, z[::-1], shading="gouraud")     # ZRCALNA SLIKA
-    plt.grid()
+    plt.pcolormesh(th - np.pi / 2, r, z[::-1], shading="gouraud")  # ZRCALNA SLIKA
+    plt.tick_params(labelcolor="white", width=0)
+    plt.tight_layout()
+    if name != "":
+        plt.savefig(name + "_real.pdf")
+    plt.show()
+
+
+    print("Imag")
+    fig = plt.figure()
+    rad = np.exp(ps)
+    azm = thetas + 3 / 2 * np.pi
+    r, th = np.meshgrid(rad, azm)
+    z = np.imag(g)
+
+    plt.subplot(projection="polar", frameon=False)
+    plt.pcolormesh(th, r, z, shading="gouraud")
+    plt.pcolormesh(th - np.pi / 2, r, z[::-1], shading="gouraud")  # ZRCALNA SLIKA
+    plt.tick_params(labelcolor="white", width=0)
+    plt.tight_layout()
+    if name != "":
+        plt.savefig(name + "_imag.pdf")
     plt.show()
 
 
@@ -24,4 +45,17 @@ def visualize_flowfield_rect(flowfield):
     plt.imshow(np.real(g))
     plt.xlabel("p = log(r/a) [0, log(R/a)]")
     plt.ylabel("thetas [0, pi/2]")
+    plt.show()
+
+
+def plot_at_constant_theta(theta, flowfield, ps, thetas):
+    theta_idx = np.argmin(np.abs(theta - thetas))
+
+    rs = np.exp(ps)
+    gs = flowfield[:, theta_idx]
+
+    plt.plot(rs, np.real(gs), 'k-')
+    plt.plot(rs, np.imag(gs), 'b--')
+    plt.xlabel(r"$r/a$", fontsize=14)
+    plt.ylabel(r"$g$    [arb.]", fontsize=14)
     plt.show()
